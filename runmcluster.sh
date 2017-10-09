ede58f39ec38faee32155ad5c095b9f908cdfaf7
Elias Rubin <erubin@princeton.edu>
2017-10-08 20:33:07 -0400

    Added script to run mcluster and create an initial distribution

---
+++ runmcluster.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/sh
+
+set -eu
+
+N=16000
+W=3
+R=1
+
+# Make initial conditions
+./mcluster -N $N -P 1 -W $W -R $R -T 50 -C 0 -G 1 -o stars
+./fixmcluster.py < stars.input > experiment.input
+mv stars.fort.10 fort.10
