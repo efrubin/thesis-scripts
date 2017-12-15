33a9e431623f919183a8aa9be4a0d7b90f7ddf21
Elias Rubin <erubin@princeton.edu>
2017-12-15 17:34:36 -0500

    testing restart.py

--- fixmcluster.py
+++ fixmcluster.py
@@ -9,7 +9,7 @@ with sys.stdin as f:
 # parameter adjustments to avoid energy problems
 lines = raw.split('\n')
 first = lines[0].split()
-lines[0] = ' '.join(map(str, [first[0], first[1], 10, 40, 40, 640]))
+lines[0] = ' '.join(map(str, [first[0], first[1], 3, 40, 40, 640]))
 four = lines[4].split()
 lines[4] = ' '.join(map(str, [four[0], four[1], four[2], 1, 2, 1, 2, 0, 3, 3]))
 five = lines[5].split()
